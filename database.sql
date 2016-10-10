38ce2a49ff66498727460cf6a1ba515b36a06220
Denis Sivzov <from.gomel@gmail.com>
2016-10-10 13:35:16 +0300

    update to karen12 from nulled

--- database.sql
+++ database.sql
@@ -1,9 +1,10 @@
-#DB Name - dropzone
+#DB Name - drop
 
-CREATE TABLE `files`
-(
-	`id` int(11) NOT NULL AUTO_INCREMENT,
-	`file_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-	`uploaded` datetime NOT NULL,
-	PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+CREATE TABLE IF NOT EXISTS `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `item_id` varchar(500) CHARACTER SET utf8 NOT NULL,
+  `file_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `uploaded` datetime NOT NULL,
+  `image_code` varchar(500) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
